eede8ad4a017d542af0e1f75c12cc6fcdab401a9
Rooted <modhanami@gmail.com>
2022-06-15 12:39:40 +0700

    Add timeout for querySelectorAllWait

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,9 +8,20 @@ export async function querySelectorWait<E extends Element>(selector: string, { d
   return element;
 }
 
-export async function querySelectorAllWait<E extends Element>(selector:string, { delay = 200 } = {}) {
+export async function querySelectorAllWait<E extends Element>(selector: string, { delay = 200, timeout = 5000 } = {}) {
   let elements: NodeListOf<E> = document.querySelectorAll(selector);
+  let reachedTimeout = false;
+
+  setTimeout(() => {
+    reachedTimeout = true;
+  }, timeout);
+
   while (!elements.length) {
+    if (reachedTimeout) {
+      console.log('querySelectorAllWait timeout');
+      break;
+    }
+
     await new Promise(resolve => setTimeout(resolve, delay));
     elements = document.querySelectorAll(selector);
   }
